ca5bc95f340ca2ed9920846ef93a8f097b9c07d8
uche solomon <solomonu928@gmail.com>
2026-02-22 00:37:18 +0100

    fix: fix ci/cd

--- examples/basics/temporary_storage/src/lib.rs
+++ examples/basics/temporary_storage/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{Env, Symbol, contract, contractimpl, contracttype, symbol_short};
+use soroban_sdk::{Env, contract, contractimpl, contracttype};
 
 #[contracttype]
 #[derive(Clone)]
